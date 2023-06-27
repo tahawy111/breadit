df2c6a045022bba04a7ba8a2284bfa1208635c19
tahawy111 <amertahawy111@gmail.com>
2023-06-27 20:47:38 +0300

    .

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,15 +1,21 @@
-"use client"
+"use client";
 import {} from "react";
-import TextAreaAutoSize from "react-textarea-autosize"
+import TextAreaAutoSize from "react-textarea-autosize";
+import { useForm } from "react-hook-form";
 
 interface EditorProps {}
 
 export default function Editor({}: EditorProps) {
+  const {} = useForm();
+
   return (
     <div className="w-full p-4 bg-zinc-50 rounded-lg border border-zinc-200">
       <form id="subreddit-post-form" className="w-fit" onSubmit={() => {}}>
         <div className="prose prose-stone dark:prose-invert">
-          <TextAreaAutoSize placeholder="Title" className="w-full resize-none appearance-none overflow-hidden bg-transparent text-5xl font-bold focus:outline-none" />
+          <TextAreaAutoSize
+            placeholder="Title"
+            className="w-full resize-none appearance-none overflow-hidden bg-transparent text-5xl font-bold focus:outline-none"
+          />
         </div>
       </form>
     </div>
---
+++ src/lib/validators/post.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const PostValidator = z.object({
+  title: z
+    .string()
+    .min(3, { message: "Title must be longer than 3 characters." })
+    .max(128, { message: "Title must be less than 128 characters" }),
+
+});
+
+export const SubredditSubscriptionValidator = z.object({
+  subredditId: z.string(),
+});
+
+// export type CreateSubredditPayload = z.infer<typeof SubredditValidator>;
+export type SubscribeToSubredditPayload = z.infer<
+  typeof SubredditSubscriptionValidator
+>;
