7d4e51851f2662017c549a6555214633718c8672
tahawy111 <amertahawy111@gmail.com>
2023-06-28 22:02:21 +0300

    Advanced caching: Optimizing post performance

--- src/app/api/subreddit/post/vote/route.ts
+++ src/app/api/subreddit/post/vote/route.ts
@@ -1,13 +1,15 @@
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { redis } from "@/lib/redis";
 import { PostVoteValidator } from "@/lib/validators/vote";
 import type { CachedPost } from "@/types/redis";
+import { z } from "zod";
 
-const CACHE_AFTER_UPVOTES = 1
+const CACHE_AFTER_UPVOTES = 1;
 
 export async function PATCH(req: Request) {
   try {
-    const body = req.json();
+    const body = await req.json();
 
     const { postId, voteType } = PostVoteValidator.parse(body);
 
@@ -46,10 +48,8 @@ export async function PATCH(req: Request) {
           },
         });
       }
-      return new Response("OK")
-    }
 
-    await db.vote.update({
+      await db.vote.update({
         where: {
           userId_postId: {
             postId,
@@ -59,25 +59,46 @@ export async function PATCH(req: Request) {
         data: {
           type: voteType,
         },
-      })
+      });
 
       // Recount the votes
       const votesAmt = post.votes.reduce((acc, vote) => {
-        if (vote.type === 'UP') return acc + 1
-        if (vote.type === 'DOWN') return acc - 1
-        return acc
-      }, 0)
+        if (vote.type === "UP") return acc + 1;
+        if (vote.type === "DOWN") return acc - 1;
+        return acc;
+      }, 0);
 
       if (votesAmt >= CACHE_AFTER_UPVOTES) {
         const cachePayload: CachedPost = {
-          authorUsername: post.author.username ?? '',
+          authorUsername: post.author.username ?? "",
           content: JSON.stringify(post.content),
           id: post.id,
           title: post.title,
           currentVote: voteType,
           createdAt: post.createdAt,
-        }
+        };
+
+        await redis.hset(`post:${postId}`, cachePayload);
+        return new Response("OK");
+      }
     }
 
-  } catch (error) {}
+    await db.vote.create({
+      data: {
+        type: voteType,
+        userId: session.user.id,
+        postId,
+      },
+    });
+
+
+    return new Response("OK")
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+        return new Response("Invalid request data passed.", { status: 422 });
+      }
+      return new Response("Could not do voting, please try again later.", {
+        status: 500,
+      });
+  }
 }
