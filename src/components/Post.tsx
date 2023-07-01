ef4dc1fddabf1e4851f9e4d5bc2ce704d69bee40
tahawy111 <amertahawy111@gmail.com>
2023-07-01 08:39:21 +0300

    deploy 3

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -54,11 +54,11 @@ export default function Post({
             <span>Posted by u/{post.author.username}</span>{" "}
             {formatTimeToNow(new Date(post.createdAt))}
           </div>
-          <a href={`/r/${subredditName}/post/${post.id}`}>
+          <Link href={`/r/${subredditName}/post/${post.id}`}>
             <h1 className="text-lg font-semibold py-2 leading-6 text-gray-900">
               {post.title}
             </h1>
-          </a>
+          </Link>
 
           <div
             className="relative text-sm max-h-40 w-full overflow-clip"
